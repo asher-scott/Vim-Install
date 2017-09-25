f81d37e00b2918504ff6d9603de79ae9d5a7547b
asher scott <asher.scott@aol.com>
2017-09-25 11:53:07 -0500

    compile command-t

--- install.py
+++ install.py
@@ -2,7 +2,12 @@
 
 import os, sys
 
-os.system("apt-get install vim git")
+os.system("apt-get install vim-nox ruby-dev")
 os.system("git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim")
 os.system("cp .vimrc ~/.vimrc")
 os.system("vim +PluginInstall +qall")
+
+
+# Set up and compile command-t
+os.system("ruby ~/.vim/bundle/command-t/ruby/command-t/ext/command-t/extconf.rb")
+os.system("make -C ~/.vim/bundle/command-t/ruby/command-t/ext/command-t")
